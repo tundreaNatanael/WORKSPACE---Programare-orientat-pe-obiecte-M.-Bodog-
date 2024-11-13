fb70f134ec377ae468641ab90208590977f8a428
nathan.tundrea <natanael.tundrea@emanuel.ro>
2024-11-13 08:48:21 +0200

    8:48 classwork

--- SongsMVC/src/main/java/ro/emanuel/songs/controller/SongsController.java
+++ SongsMVC/src/main/java/ro/emanuel/songs/controller/SongsController.java
@@ -3,6 +3,7 @@ package ro.emanuel.songs.controller;
 import java.sql.SQLException;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,8 +14,11 @@ import ro.emanuel.songs.pojo.Song;
 public class SongsController {
 
 	@GetMapping("/song")
-	public String singleSong(@RequestParam int id) throws SQLException {
+	public String singleSong(@RequestParam int id, Model model) throws SQLException {
 		Song song = SongDAO.getById(id);
+		
+		model.addAttribute("s", song);
+		
 		return "song.jsp";
 	}
 }
